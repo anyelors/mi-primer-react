d35c132ecb4c59d9fe60af425860db71724a4a50
Anyelo Ramirez <anyelors@gmail.com>
2025-07-07 20:09:27 +0200

    Clase React #2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Saludo from './components/saludo.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hola</h1>
+        <Saludo nombre="Lupe" />
+        <h2>Primer proyecto React</h2>
       </header>
     </div>
   );
---
+++ src/components/saludo.jsx
@@ -0,0 +1,5 @@
+function Saludo({ nombre }) {
+  return <h2>Ongi etorri, {nombre}!</h2>;
+}
+
+export default Saludo;
